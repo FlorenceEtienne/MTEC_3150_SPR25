8eae828ce23ec02a8ee1917d97955663155a098c
FuzzWil <florence.etienne@mail.citytech.cuny.edu>
2025-02-17 17:25:40 -0500

    re-added to test scene

--- Project1/Scattered_Entropy/Assets/Scripts/player.cs
+++ Project1/Scattered_Entropy/Assets/Scripts/player.cs
@@ -5,20 +5,39 @@ using UnityEngine;
 public class player : MonoBehaviour
     
 {
-    public float SpeedX;
-    public float SpeedY;
+    // public shows any variable or function 
+    // into the editor (usually uppercase them to know the difference)
+
+    //float variable that assigns the vertical speed(movement) of player
+    public float SpdX;
+
+    // float variable that assigns the horizontal speed (movement) of player
+    public float SpdY;
     public string InputAxisNameVert;
-    public string InputAxisNameHori;   
+    public string InputAxisNameHori;
+
+    public Rigidbody2D r2d;   
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // anything that uses () that isn't a function is a method
+        r2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // create a float variable that gets input strings' value 
+        // from the input manager in the editor
+        float directionVert = Input.GetAxis(InputAxisNameVert);
+        float directionHori = Input.GetAxis(InputAxisNameHori);
+
+        // a 2D vector (x,y) variable that assigns a new Vector2 for the velocity
+        Vector2 vel = new Vector2(directionHori, directionVert);
+
+        // . calls any properties associated with variables, methods, etc.
+        vel.x *= SpdX;
+        vel.y *= SpdY;
     }
 }
