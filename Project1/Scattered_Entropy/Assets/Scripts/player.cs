c72b872638bc92e55da8a5b7ba6f1690d4c32d7f
FlorenceEtienne <florence.etienne@mail.citytech.cuny.edu>
2025-02-17 09:30:52 -0500

    Added to testing scene

---
+++ Project1/Scattered_Entropy/Assets/Scripts/player.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+    
+{
+    public float SpeedX;
+    public float SpeedY;
+    public string InputAxisNameVert;
+    public string InputAxisNameHori;   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
